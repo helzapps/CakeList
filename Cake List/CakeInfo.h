95f4de462d3c7e4c85d84cce77db0e8c301b15b5
Jordan Helzer <jhelzer@infusemed.com>
2018-04-04 00:35:02 -0700

    Created model object for cake info

---
+++ Cake List/CakeInfo.h
@@ -0,0 +1,13 @@
+//
+//  CakeInfo.h
+//  Cake List
+//
+//  Created by Jordan Helzer on 4/4/18.
+//  Copyright © 2018 Stewart Hart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CakeInfo : NSObject
+
+@end
---
+++ Cake List/CakeInfo.m
@@ -0,0 +1,13 @@
+//
+//  CakeInfo.m
+//  Cake List
+//
+//  Created by Jordan Helzer on 4/4/18.
+//  Copyright © 2018 Stewart Hart. All rights reserved.
+//
+
+#import "CakeInfo.h"
+
+@implementation CakeInfo
+
+@end
