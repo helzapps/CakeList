f260accf8bb12caf69af724e8c6b4895ae187b1a
Jordan Helzer <jhelzer@infusemed.com>
2018-04-04 22:13:32 -0700

    Implemented guts of ImageCache to queue up image downloads but only accept one request per image URL and then to queue up any completion blocks passed in. Lastly when the image downloads or fails, execute the completion blocks. Note: cache could be expanded to an operationQueue with progress updates and controlled concurrency, in addition to expansion into potentially a least recently used cache to throw out images not used in order to maintain memory usage requirements

--- Cake List/ImageCache.swift
+++ Cake List/ImageCache.swift
@@ -9,5 +9,64 @@
 import UIKit
 
 class ImageCache: NSObject {
+	
+	static let shared = ImageCache()
+	private var cache: [String : CacheObject] = [:]
+	
+	class func getImage(at url: URL, completionHandler: @escaping CacheObject.CompletionBlock) {
+		if let cacheObject = shared.cache[url.absoluteString] {
+			if let image = cacheObject.image {
+				return completionHandler(image, nil)
+			}
+			cacheObject.completionBlocks.append(completionHandler)
+		} else {
+			let cacheObject = CacheObject()
+			cacheObject.completionBlocks.append(completionHandler)
+			shared.cache[url.absoluteString] = cacheObject
+			shared.downloadImage(at: url) { (image: UIImage?, error: Error?) in
+				guard let cacheObject = ImageCache.shared.cache[url.absoluteString] else {
+					return
+				}
+				cacheObject.image = image
+				for block in cacheObject.completionBlocks {
+					block(image, error)
+				}
+				cacheObject.completionBlocks.removeAll()
+			}
+		}
+	}
+	
+	class func getImage(at urlString: String) -> UIImage? {
+		var image: UIImage?
+		if let cacheObject = shared.cache[urlString] {
+			image = cacheObject.image
+		}
+		return image
+	}
+	
+	private func downloadImage(at url: URL, completionHandler: @escaping CacheObject.CompletionBlock) {
+		DispatchQueue.global(qos: .background).async {
+			var image: UIImage?
+			var anError: Error?
+			defer {
+				DispatchQueue.main.async {
+					completionHandler(image, anError)
+				}
+			}
+			do {
+				let imageData = try Data.init(contentsOf: url)
+				image = UIImage(data: imageData)
+			} catch {
+				anError = error
+			}
+		}
+	}
+	
+}
 
+class CacheObject: NSObject {
+	typealias CompletionBlock = (_ image: UIImage?,_ error: Error?)->Void
+	
+	var image: UIImage?
+	var completionBlocks: [CompletionBlock] = []
 }
