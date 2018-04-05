e5ba3cbd20878ef97af1ed5efbf60d4eceb39d0a
Jordan Helzer <jhelzer@infusemed.com>
2018-04-04 22:08:16 -0700

    Created ImageCache swift file

---
+++ Cake List/ImageCache.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCache.swift
+//  Cake List
+//
+//  Created by Jordan Helzer on 4/4/18.
+//  Copyright © 2018 Stewart Hart. All rights reserved.
+//
+
+import UIKit
+
+class ImageCache: NSObject {
+
+}
